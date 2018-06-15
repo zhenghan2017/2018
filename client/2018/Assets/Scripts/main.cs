ae8682b57b8a08f34ae0d9f255cae5f367c3c36f
zhengyuhang <zhengyuhang@bianfeng.com>
2018-06-15 20:40:19 +0800

    消息框架测试

--- client/2018/Assets/Scripts/main.cs
+++ client/2018/Assets/Scripts/main.cs
@@ -8,7 +8,30 @@ public class main : MonoBehaviour {
 
     void Start()
     {
+        //Messenger.Broadcast("Send"); 调用的方式 
+        //Messenger.Broadcast("Send",GameObject); 带参数的调用方式
+        MessageCenter.AddListener("Msg_GameStart", DoSomething);
+
+        MessageCenter.AddListener<GameObject>("1212", DoSomething);
         //加载登陆窗体
         UIManager.GetInstance().ShowUIForms(UIDefine.LOGON_FROMS);
+    }
+
+    public void DoSomething()
+    {
+        Debug.Log("进入主场景界面");
+    }
+
+    public void DoSomething(GameObject obj)
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            MessageCenter.Broadcast("Msg_GameStart");
+        }
     }
 }
