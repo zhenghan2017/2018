0a73f0f4c78ebe2863c2d4518a3f1710b0e6578c
zhenghan2017 <17826826870@163.com>
2018-07-12 17:28:09 +0800

    数据库建表语句更新-用户表增加联系方式字段，后续方便扩展

--- server/数据库建表语句.sql
+++ server/数据库建表语句.sql
@@ -5,9 +5,10 @@ CREATE TABLE IF NOT EXISTS `User` (
   `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `password` varchar(50) COLLATE utf8_unicode_ci DEFAULT '',
+  `contact` varchar(25) DEFAULT NULL,
   `loginCount` smallint(6) unsigned DEFAULT '0',
   `lastLoginIp` varchar(25) COLLATE utf8_unicode_ci DEFAULT NULL,
-  `lastLoginTime` date unsigned DEFAULT '0',
+  `lastLoginTime` date,
   PRIMARY KEY (`id`),
   UNIQUE KEY `INDEX_ACCOUNT_NAME` (`name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
